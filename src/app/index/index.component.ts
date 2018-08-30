94dc65d58d3380286d31a0682d75396c4e27916b
agungdh <agungdh@live.com>
2018-08-30 12:56:18 +0700

    update index component

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -20,7 +20,49 @@ export class IndexComponent implements OnInit {
 
   ambilMahasiswa() {
     this.service.ambilMahasiswa().subscribe(res => {
-      this.mahasiswa = res;
+      	let semuaMahasiswa: object[] = [];
+	    for (let keyRes in res) {
+	    	let satuMahasiswa: {} = res[keyRes];
+
+	    	var satuMahasiswaJadi:any = {};
+	    	for (let keySatuMahasiswa in satuMahasiswa) {
+	    		switch (keySatuMahasiswa) {
+	    			case "npm":
+	    				satuMahasiswaJadi.npm = satuMahasiswa[keySatuMahasiswa];
+	    				break;
+	    			
+	    			case "nama":
+	    				satuMahasiswaJadi.nama = satuMahasiswa[keySatuMahasiswa];
+	    				break;
+	    			
+	    			case "alamat":
+	    				satuMahasiswaJadi.alamat = satuMahasiswa[keySatuMahasiswa];
+	    				break;
+	    			
+	    			case "tanggallahir":
+	    				satuMahasiswaJadi.tanggallahir = satuMahasiswa[keySatuMahasiswa];
+	    				break;
+	    			
+	    			case "jeniskelamin":
+	    				if (satuMahasiswa[keySatuMahasiswa] == 'l') {
+	    					satuMahasiswaJadi.jeniskelamin = 'Perempuan';
+	    				} else if (satuMahasiswa[keySatuMahasiswa] == 'p') {
+	    					satuMahasiswaJadi.jeniskelamin = 'Laki-Laki';
+	    				} else {
+	    					satuMahasiswaJadi.jeniskelamin = 'ERROR !!!';
+	    				}
+	    				break;
+	    			
+	    			default:
+	    				
+	    				break;
+	    		}
+	    	}
+    		semuaMahasiswa.push(satuMahasiswaJadi);
+		}
+
+      this.mahasiswa = semuaMahasiswa;
+
     });
   }
 }
